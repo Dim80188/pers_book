e7e6a2f8736b861bfc3f984acf5ffb4aa8ded0bc
Dim80188 <8914843.pc@gmail.com>
2022-09-30 16:40:22 +0300

    Models3

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,16 +15,19 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''тест: можно сохранить post-запрос'''
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+
+
+    def test_redirects_after_POST(self):
+        '''тест: переадресует после post-запроса'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
-        # self.assertIn('A new list item', response.content.decode())
-        # self.assertTemplateUsed(response, 'home.html')
 
     def test_only_saves_items_when_necessary(self):
         '''тест: сохраняет элементы, только когда нужно'''
