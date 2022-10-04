de1b63272c1a7adf9b52449742940efb1dbcbf14
Dim80188 <8914843.pc@gmail.com>
2022-10-04 09:39:37 +0300

    func_test_fin

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
@@ -25,6 +26,7 @@ class NewVisitorTest(LiveServerTestCase):
                 table = self.browser.find_element(By.ID, 'id_list_table')
                 rows = table.find_elements(By.TAG_NAME, 'tr')
                 self.assertIn(row_text, [row.text for row in rows])
+
                 return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
